2013069212b35cbd02a13ecbd0bae276efa3642b
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-07 12:25:47 +0100

    added puppeteer

--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -16,12 +16,31 @@ export default function (cb) {
         let response = null
 
         try {
-            //response = await axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE')
-            //console.log(response.data.listMap)
             const response = await fetch('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE');
             const data = await response.json();
            
-            console.log(data);
+            console.log(data.listMap);
+
+            await callback(null)
+        } catch (error) {
+            console.log(error)
+            clearInterval(timer1)
+        }
+
+    }, 5000, cb)
+
+
+    timer2 = setInterval(async function (callback) {
+
+        console.log('timer 2...')
+
+        let response = null
+
+        try {
+            const response = await fetch('https://api.store.nvidia.com/partner/v1/feinventory?skus=NL~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=NL');
+            const data = await response.json();
+           
+            console.log(data.listMap);
 
             await callback(null)
         } catch (error) {
