3e167c6129ae7390223f9a8b258f9a015b91df97
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-07 15:14:04 +0100

    added puppeteer

--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -8,7 +8,6 @@ export default function (cb) {
 
     console.log('bot started...')
 
-
     timer1 = setInterval(async function (callback) {
 
         console.log('timer 1...')
@@ -30,7 +29,7 @@ export default function (cb) {
             response = await fetch('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE');
             data = await response.json();
             
-            products = data.listMap.filter(function (product) { return skus.includes(product.fe_sku ) && product.is_active == 'true' })
+            products = data.listMap.filter(function (product) { return skus.includes(product.fe_sku) && product.is_active == 'true' })
             
             while (x <= products.length) {
                 await callback(products[x])
@@ -65,7 +64,7 @@ export default function (cb) {
             response = await fetch('https://api.store.nvidia.com/partner/v1/feinventory?skus=NL~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=NL');
             data = await response.json()
            
-            products = data.listMap.filter(function (product) { return skus.includes(product.fe_sku ) && product.is_active == 'true' })
+            products = data.listMap.filter(function (product) { return skus.includes(product.fe_sku) && product.is_active == 'true' })
             
             while (x <= products.length) {
                 await callback(products[x])
@@ -73,10 +72,8 @@ export default function (cb) {
             }
         } catch (error) {
             console.log(error)
-            clearInterval(timer1)
+            clearInterval(timer2)
         }
 
     }, 50000, cb)
-
-   
 }
