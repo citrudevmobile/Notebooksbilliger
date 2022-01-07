5503b04948b4fa46434dbe9bc7c48dc1f382e58f
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-07 07:34:27 +0100

    added puppeteer

--- bot/index.js
+++ bot/index.js
@@ -1,6 +1,19 @@
 const storeUrl = `https://www.notebooksbilliger.de/`
 const monitorAPI = require('./libs/monitorAPI')
+const puppeteer = require('puppeteer-core')
 
-monitorAPI(function(found) {
+monitorAPI(async function(found) {
+
     console.log("product found start add to cart and checkout task...")
+
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/google-chrome'
+    })
+
+    const page = await browser.newPage()
+    await page.goto(storeUrl)
+    await page.waitForTimeout(500000)
+
+    
 })
