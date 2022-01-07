7d6a7aad6773d44066ceeb5d5b929e563ab9d012
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-07 08:41:02 +0100

    added puppeteer

--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -7,6 +7,17 @@ module.exports = function (cb) {
 
     console.log('bot started...')
 
+    axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE').then(function (response) {
+            
+        console.log(response.data.listMap)
+        cb(null)
+       
+    }).catch(function (error) {
+
+        console.log(error)
+        clearInterval(timer1)
+    })
+
     timer1 = setInterval(function (callback) {
         console.log('timer 1...')
         axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE').then(function (response) {
