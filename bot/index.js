62b0a95797ab85bc0d5e7138a2588b069d50224e
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-07 02:12:47 +0100

    added proxy module

--- bot/index.js
+++ bot/index.js
@@ -2,5 +2,5 @@ const storeUrl = `https://www.notebooksbilliger.de/`
 const monitorAPI = require('./libs/monitorAPI')
 
 monitorAPI(function(found) {
-    console.log("product found start auotcart task...")
+    console.log("product found start add to cart and checkout task...")
 })
--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -1,11 +1,11 @@
 const axios = require('axios')
 
 module.exports = function (cb) {
-    
+
     let timer1 = null
     let timer2 = null
 
-    timer1 = setinterval(function (callback) {
+    timer1 = setInterval(function (callback) {
         axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE').then(function (response) {
             
             console.log(response.data.listMap)
@@ -19,7 +19,7 @@ module.exports = function (cb) {
 
     }, 10000, cb)
 
-    timer2 = setinterval(function (callback) {
+    timer2 = setInterval(function (callback) {
         axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=NL~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=NL').then(function (response) {
            
             console.log(response.data.listMap)
