10be7d816fd5224162e195dacbed55e18078c52b
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-07 02:08:20 +0100

    added proxy module

--- bot/index.js
+++ bot/index.js
@@ -1,1 +1,6 @@
 const storeUrl = `https://www.notebooksbilliger.de/`
+const monitorAPI = require('./libs/monitorAPI')
+
+monitorAPI(function(found) {
+    console.log("product found start auotcart task...")
+})
--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -1,24 +1,35 @@
 const axios = require('axios')
 
 module.exports = function (cb) {
+    
     let timer1 = null
     let timer2 = null
 
     timer1 = setinterval(function (callback) {
         axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE').then(function (response) {
-            console.log(response);
+            
+            console.log(response.data.listMap)
+            callback(null)
+           
         }).catch(function (error) {
-            console.log(error);
+
+            console.log(error)
+            clearInterval(timer1)
         })
 
-    }, 1000, cb)
+    }, 10000, cb)
 
     timer2 = setinterval(function (callback) {
         axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=NL~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=NL').then(function (response) {
-            console.log(response);
+           
+            console.log(response.data.listMap)
+            callback(null)
+
         }).catch(function (error) {
-            console.log(error);
+
+            console.log(error)
+            clearInterval(timer2)
         })
 
-    }, 1000, cb)
+    }, 100000, cb)
 }
