1dc7904c1f2f44b3482e64d348dd430bb7f7862e
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-08 21:29:27 +0100

    fixing chrome

--- bot/index.js
+++ bot/index.js
@@ -1,19 +1,26 @@
 const storeUrl = `https://www.notebooksbilliger.de/`
 import monitorAPI  from './libs/monitorAPI.js'
 import puppeteer from 'puppeteer'
+import executionTime from 'execution-time'
+
+let execTimer = executionTime(console.log)
+
 
 monitorAPI(async function(found) {
-
+   
     console.log("product found start add to cart and checkout task...")
 
     console.log(found)
     
     const browser = await puppeteer.launch({
         headless: false,
+        executablePath: `C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe`
     })
 
     const page = await browser.newPage()
+    execTimer.start()
     await page.goto(storeUrl)
+    execTimer.stop()
     await page.waitForTimeout(500000)
     
 })
