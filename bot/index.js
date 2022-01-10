b7fc41beebd1224718bc4c8a13f385d3444703c2
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-10 22:21:43 +0100

    fixed monitor api

--- bot/index.js
+++ bot/index.js
@@ -27,9 +27,9 @@ Workers(async function(pubsub) {
         workerName: 'worker2',
         userEmail: 'nemeth.judit8806@gmail.com',
         userPassword: 'QWERT12345',
-        proxyServer: 'deu.resi.dreamproxies.io:26208',
+        proxyServer: 'deu.resi.dreamproxies.io:26329',
         proxyUser: '6M1mec8j',
-        proxyPassword: 'Q2W2Y50dh7RJDfP6NaCg9u9n66tRdpykRLO74Ob23hBoZryeLYc6Q1qdlrXgPB2MY3xji-JCgrP9QobC'
+        proxyPassword: 'Q2W2Y50dh7RJDfP6NaCg9u9n66tRdpykRLO74Ob23hBoZryeLYc6Q1qdlrXgPB2MY3xji-4HlCrWPSq4'
     })
 
     monitorAPI(pubsub, async function(found) {
