f79d1089f1635fea43bba9e6667c46023cc2d84b
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-10 16:03:06 +0100

    fixed monitor api

--- bot/index.js
+++ bot/index.js
@@ -9,6 +9,9 @@ Workers(async function(pubsub) {
     pubsub.subscribe('ready_worker', function (data) {
         console.log(`Worker ${data.workerName} is ready`)
         readyWorkers.push(data.workerName)
+        if(readyWorkers.length == 0) {
+            pubsub.publish('monitor_api')
+        }
     })
 
     pubsub.publish('start_worker', {
@@ -29,8 +32,7 @@ Workers(async function(pubsub) {
         proxyPassword: 'Q2W2Y50dh7RJDfP6NaCg9u9n66tRdpykRLO74Ob23hBoZryeLYc6Q1qdlrXgPB2MY3xji-JCgrP9QobC'
     })
 
-    monitorAPI(async function(found) {
-        console.log(found)
+    monitorAPI(pubsub, async function(found) {
         try {
             let readyWorker = readyWorkers.shift()
             if (readyWorker) {
--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -1,99 +1,106 @@
 import axios from 'axios-https-proxy-fix'
 
-export default function (cb) {
+export default function (pubsub, cb) {
 
     let timer1 = null
     let timer2 = null
 
     console.log('bot started...')
 
-    timer1 = setInterval(async function (callback) {
+    pubsub.subscribe('monitor_api', async (data) => { 
+        console.log('monitor api started...')
+        timer1 = setInterval(async function (callback) {
 
-        console.log('timer 1...')
-
-        let response = null
-        let products = []
-        let data = null
-        let skus = [
-        "NVGFT070T_DE", 
-        "NVGFT080_DE",
-        "NVGFT070_DE",
-        "NVGFT060T_DE",
-        "NVGFT080T_DE"
-        ]
-        let x = 0
-        //https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE', { agent: new HttpsProxyAgent('https://Bj9VYo37X7JbdEOs_country-Germany_session-iD37eQWS:yzor1c6scv@basic.dreamproxies.io:31112
-        //https://www.mockachino.com/27bcb9bb-1e22-45/users
-        try {
-
-            response = await axios.get('https://www.mockachino.com/27bcb9bb-1e22-45/users', {
-            /*    
-            proxy: {
-                    protocol:'http',
-                    host: 'basic.dreamproxies.io',
-                    port: '31112',
-                    auth: {
-                      username: 'yzor1c6scv',
-                      password: 'Bj9VYo37X7JbdEOs_country-Germany'
-                    }
-                  }
-                  */
-            })
-            
-            products = response.data.listMap.filter(function (product) { return skus.includes(product.fe_sku) && product.is_active == 'true' })
-            
-            while (x < products.length) {
-                callback(products[x])
-                x++
-            }
-
-            if (products.length) {
+            console.log('timer 1...')
+    
+            let response = null
+            let products = []
+            let data = null
+            let skus = [
+            "NVGFT070T_DE", 
+            "NVGFT080_DE",
+            "NVGFT070_DE",
+            "NVGFT060T_DE",
+            "NVGFT080T_DE"
+            ]
+            let x = 0
+            //https://api.store.nvidia.com/partner/v1/feinventory?skus=DE~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=DE', { agent: new HttpsProxyAgent('https://Bj9VYo37X7JbdEOs_country-Germany_session-iD37eQWS:yzor1c6scv@basic.dreamproxies.io:31112
+            //https://www.mockachino.com/27bcb9bb-1e22-45/users
+            try {
+    
+                response = await axios.get('https://www.mockachino.com/27bcb9bb-1e22-45/users', {
+                /*    
+                proxy: {
+                        protocol:'http',
+                        host: 'basic.dreamproxies.io',
+                        port: '31112',
+                        auth: {
+                          username: 'yzor1c6scv',
+                          password: 'Bj9VYo37X7JbdEOs_country-Germany'
+                        }
+                      }
+                      */
+                })
+                
+                products = response.data.listMap.filter(function (product) { return skus.includes(product.fe_sku) && product.is_active == 'true' })
+                
+                while (x < products.length) {
+                    callback(products[x])
+                    x++
+                }
+    
+                if (products.length) {
+                    clearInterval(timer1)
+                    console.log('timer1 stopped...')    
+                }
+    
+            } catch (error) {
+                console.log(error)
                 clearInterval(timer1)
-                console.log('timer1 stopped...')    
             }
-
-        } catch (error) {
-            console.log(error)
-            clearInterval(timer1)
-        }
-
-    }, 50000, cb)
-
-
-    timer2 = setInterval(async function (callback) {
-
-        console.log('timer 2...')
-
-        let response = null
-        let products = []
-        let data = null
-        let skus = [
-        "NVGFT070T_DE", 
-        "NVGFT080_DE",
-        "NVGFT070_DE",
-        "NVGFT060T_DE",
-        "NVGFT080T_DE"
-        ]
-        let x = 0
-
-        try {
-            response = await axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=NL~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=NL');
+    
+        }, 50000, cb)
+    
+    
+        timer2 = setInterval(async function (callback) {
+    
+            console.log('timer 2...')
+    
+            let response = null
+            let products = []
+            let data = null
+            let skus = [
+            "NVGFT070T_DE", 
+            "NVGFT080_DE",
+            "NVGFT070_DE",
+            "NVGFT060T_DE",
+            "NVGFT080T_DE"
+            ]
+            let x = 0
+    
+            try {
+                response = await axios.get('https://api.store.nvidia.com/partner/v1/feinventory?skus=NL~NVGFT070~NVGFT080~NVGFT090~NVLKR30S~NSHRMT01~NVGFT060T~187&locale=NL');
+                
+                products = response.data.listMap.filter(function (product) { return skus.includes(product.fe_sku) && product.is_active == 'false' })
             
-            products = response.data.listMap.filter(function (product) { return skus.includes(product.fe_sku) && product.is_active == 'false' })
-        
-            while (x < products.length) {
-                callback(products[x])
-                x++
+                while (x < products.length) {
+                    callback(products[x])
+                    x++
+                }
+    
+                if (products.length) {
+                    clearInterval(timer2) 
+                    console.log('timer2 stopped...')  
+                }
+            } catch (error) {
+                console.log(error)
+                clearInterval(timer2)
             }
+    
+        }, 50000, cb)
 
-            if (products.length) {
-                clearInterval(timer2) 
-                console.log('timer2 stopped...')  
-            }
-        } catch (error) {
-            console.log(error)
-            clearInterval(timer2)
-        }
 
-    }, 50000, cb)
+    })
+
+  
 }
