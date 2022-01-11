48f9401120193168fe30acb8984454e24aab88f3
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-11 02:33:58 +0100

    save chnages

--- bot/tasks.js
+++ bot/tasks.js
@@ -3,9 +3,9 @@ export default [
         workerName: 'worker1',
         userEmail: 'monyo.kapa88@gmail.com',
         userPassword: 'QWERT12345',
-        proxyServer: 'deu.resi.dreamproxies.io:26371',
+        proxyServer: 'deu.resi.dreamproxies.io:26258',
         proxyUser: '6M1mec8j',
-        proxyPassword: 'Q2W2Y50dh7RJDfP6NaCg9u9n66tRdpykRLO74Ob23hBoZryeLYc6Q1qdlrXgPB2MY3xji-3VDrjf4P4Z'
+        proxyPassword: 'Q2W2Y50dh7RJDfP6NaCg9u9n66tRdpykRLO74Ob23hBoZryeLYc6Q1qdlrXgPB2MY3xji-9oucfSC9KO'
     },
     {
         workerName: 'worker2',
