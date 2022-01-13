bf9ca0f66b7daec21e48ec9972df4fb7a9dd4c1d
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-13 22:22:57 +0100

    save chnages

--- bot/libs/workers.js
+++ bot/libs/workers.js
@@ -75,6 +75,7 @@ export default function (cb) {
                         await page.waitForSelector('#haccount',{ timeout: 100000 })
                         break;
                     } catch (error) {
+                        console.log(error)
                         console.log(`${workerName} retry login...`)
                     }
                     x++
