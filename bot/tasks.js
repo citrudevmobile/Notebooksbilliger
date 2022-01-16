b1c9a23502fe862cb929df79422127d5a6b0b4ef
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-16 14:51:06 +0100

    save chnages

--- bot/tasks.js
+++ bot/tasks.js
@@ -5,7 +5,7 @@ export default [
         userPassword: 'QWERT12345',
         proxyServer: 'basic.dreamproxies.io:31112',
         proxyUser: 'yzor1c6scv',
-        proxyPassword: 'Bj9VYo37X7JbdEOs_country-Germany_session-duHLlJom'
+        proxyPassword: 'Bj9VYo37X7JbdEOs_country-Germany_session-JZiXpdUo'
     },
     {
         workerName: 'worker2',
@@ -13,6 +13,6 @@ export default [
         userPassword: 'QWERT12345',
         proxyServer: 'basic.dreamproxies.io:31112',
         proxyUser: 'yzor1c6scv',
-        proxyPassword: 'Bj9VYo37X7JbdEOs_country-Germany_session-wwxATz5t'
+        proxyPassword: 'Bj9VYo37X7JbdEOs_country-Germany_session-jjGobsHH'
     }
 ]
