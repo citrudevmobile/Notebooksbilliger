513bec8a8c266fa04167d451ccfe7245995225b8
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-19 02:07:06 +0100

    save chnages

--- bot/libs/proxyrotation.js
+++ bot/libs/proxyrotation.js
@@ -4,31 +4,13 @@ const freeProxies = [
 
 ]
 
-const paidProxies = [
-    'basic.dreamproxies.io:31112:yzor1c6scv:Bj9VYo37X7JbdEOs_country-Germany_session-iD37eQWS',
-    'basic.dreamproxies.io:31112:yzor1c6scv:Bj9VYo37X7JbdEOs_country-Germany_session-h63PGUCv',
-    'basic.dreamproxies.io:31112:yzor1c6scv:Bj9VYo37X7JbdEOs_country-Germany_session-ExoQ7bKN',
-    'basic.dreamproxies.io:31112:yzor1c6scv:Bj9VYo37X7JbdEOs_country-Germany_session-gpeTDs56',
-    'basic.dreamproxies.io:31112:yzor1c6scv:Bj9VYo37X7JbdEOs_country-Germany_session-Pnpw4lRK',
-    'basic.dreamproxies.io:31112:yzor1c6scv:Bj9VYo37X7JbdEOs_country-Germany_session-VwgV6ax8']
-
-let lastRandNumForPaidProxies = 0
-
 let lastRandNumForFreeProxies = 0
 
 
-fs.readfile('', 'utf8', function(err,data) {
+fs.readfile('proxies.txt', 'utf8', function(err,data) {
     if(err) throw err;
-    let obj = {};
-    let splitted = data.toString().split("\n");
-    for (let i = 0; i<splitted.length; i++) {
-        let splitLine = splitted[i].split(":");
-        obj[splitLine[0]] = splitLine[1].trim();
-    }
-    console.log(obj);
+    console.log(data)
 })
-
-
 
 
 let generateRandNum  = function (proxyArray) {
@@ -36,8 +18,7 @@ let generateRandNum  = function (proxyArray) {
 }
 
 
-// Rotate free proxies
-let rotateFreeProxies = function () {
+let rotateProxies = function () {
     let randNum = generateRandNum(freeProxies)
     if (lastRandNumForFreeProxies != randNum) {
         lastRandNumForFreeProxies = randNum
@@ -49,21 +30,7 @@ let rotateFreeProxies = function () {
     }
 }
 
-// Rotate paid proxies
-let rotatePaidProxies = function () {
-    let randNum = generateRandNum(paidProxies)
-    if (lastRandNumForPaidProxies != randNum) {
-        lastRandNumForPaidProxies = randNum
-        return paidProxies[randNum]
-    } else {
-        randNum = generateRandNum(paidProxies)
-        lastRandNumForPaidProxies = randNum
-        return paidProxies[randNum]
-    }    
-}
-
 
 export {
-    rotateFreeProxies,
-    rotatePaidProxies
+    rotateProxies
 }
