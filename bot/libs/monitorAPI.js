9e22890d1817407ad469dd35a9ee2eaaf12420e5
Evans Enonchong <citrudevmobile@gmail.com>
2022-01-19 07:46:38 +0100

    save changes

--- bot/libs/monitorAPI.js
+++ bot/libs/monitorAPI.js
@@ -14,7 +14,7 @@ export default function (pubsub, cb) {
     pubsub.subscribe('monitor_api', async (data) => {  
 
         console.log('Monitoring API...')
-        await page.waitForSelector(`form[name="checkout_summary_form"]`, { timeout: 100000 })
+       
         timer1 = setInterval(async function (callback) {
 
             console.log('API 1 request...')
