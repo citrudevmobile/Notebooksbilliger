cd69a143106d4a1dd63e3a91d2e30a1ab05ce477
Badrick16 <91611008+Badrick16@users.noreply.github.com>
2022-02-08 18:09:59 +0100

    date stamp added

--- bot/libs/logger.js
+++ bot/libs/logger.js
@@ -1,10 +1,19 @@
 import { Console } from "console";
 import fs from "fs";
 
+const date = new Date();
+const year = String(date.getFullYear()).padStart(2, "0");
+const month = String(date.getMonth()).padStart(2, "0");
+const day = String(date.getDay()).padStart(2, "0");
+const hours = String(date.getHours()).padStart(2, "0");
+const minutes = String(date.getMinutes()).padStart(2, "0");
+const seconds = String(date.getSeconds()).padStart(2, "0");
+
 const logger = new Console({
+    // '`log_${new Date(Date.now()).toLocaleString("en-US")}.txt`;
     stdout: fs.createWriteStream(
-        `log_${new Date(Date.now()).toLocaleString("en-US")}.txt`
+        `log_${year}${month}${day}_${hours}${minutes}${seconds}.txt`
     ),
 });
 
-export default logger;
+export default logger; 
