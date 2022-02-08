9db9195b1cc4f852ed929aae8da65ebfc2c87c92
Badrick16 <91611008+Badrick16@users.noreply.github.com>
2022-02-08 12:23:07 +0100

    Added environment variables to not overwrite the logfile

--- bot/libs/logger.js
+++ bot/libs/logger.js
@@ -2,7 +2,7 @@ import { Console } from 'console'
 import fs from "fs"
 
 const logger = new Console({
-    stdout: fs.createWriteStream("log.txt")
+    stdout: fs.createWriteStream(`log_${(new Date().toJSON().slice(0,19))}.txt`)
 })
 
 export default logger
