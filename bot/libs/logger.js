279edf9145f99de9499b82dafbbf60f9e6cd072e
Badrick16 <91611008+Badrick16@users.noreply.github.com>
2022-02-08 13:16:45 +0100

    Update logger.js

--- bot/libs/logger.js
+++ bot/libs/logger.js
@@ -1,8 +1,10 @@
-import { Console } from 'console'
-import fs from "fs"
+import { Console } from "console";
+import fs from "fs";
 
 const logger = new Console({
-    stdout: fs.createWriteStream(`log_${(new Date().toJSON().slice(0,19))}.txt`)
-})
+    stdout: fs.createWriteStream(
+        `log_${new Date(Date.now()).toLocaleString("en-US")}.txt`
+    ),
+});
 
-export default logger
+export default logger;
